4b49c022bbfe80738bb6d8d3279bde5370b88cd7
jcdiazdelcastillo <147615359+jcdiazdelcastillo@users.noreply.github.com>
2025-03-24 10:10:58 +0100

    Update configdb.php

--- Proyecto Jesuitas/anadirJesuitas/configdb.php
+++ Proyecto Jesuitas/anadirJesuitas/configdb.php
@@ -1,17 +1,6 @@
 
 
 <?php
-
-/* HOSTING*/
-
-/*define("SERVIDOR",'1daw.esvirgua.com');
-define("USUARIO",'user1daw_07');
-define("PASSWORD",'hIps{MkMft9M');
-define("BBDD",'user1daw_BD1-07');*/
-
-
-
-
 /* LOCAL*/
 
 define("SERVIDOR",'localhost');
