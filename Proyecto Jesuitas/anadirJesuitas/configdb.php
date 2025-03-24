4a76fc2d67ba630ea2475898b96d09f59522ccab
jcdiazdelcastillo <147615359+jcdiazdelcastillo@users.noreply.github.com>
2025-03-24 10:10:18 +0100

    Add files via upload

---
+++ Proyecto Jesuitas/anadirJesuitas/configdb.php
@@ -0,0 +1,22 @@
+
+
+<?php
+
+/* HOSTING*/
+
+/*define("SERVIDOR",'1daw.esvirgua.com');
+define("USUARIO",'user1daw_07');
+define("PASSWORD",'hIps{MkMft9M');
+define("BBDD",'user1daw_BD1-07');*/
+
+
+
+
+/* LOCAL*/
+
+define("SERVIDOR",'localhost');
+define("USUARIO",'root');
+define("PASSWORD",'');
+define("BBDD",'jesuitas');
+
+?>
